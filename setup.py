fb8390ec726a088a6174705d658a4ea8f69d47c6
Abid Hasan Mahmud <abid.hasanmahmud@gmail.com>
2025-04-15 14:47:08 +0600

    third commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'QApplication',
     version= '0.0.1',
-    author= 'sunny savita',
-    author_email= 'sunny.savita@gmail.com',
+    author= 'abid hasan mahmud',
+    author_email= 'abid.hasanmahmud@gmail.com',
     packages= find_packages(),
     install_requires = []
 
